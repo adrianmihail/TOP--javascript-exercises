b7df5c3c19468ab3e0860c2bf5a0e49be70e46ed
adrianmihail <adrianmihail00@gmail.com>
2023-12-29 13:49:11 +0200

    Finish it

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(number) {
+  return number.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(number) {
+  return number.reduce((sum, current) => sum * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let arr = [];
+
+  for (let i = 1; i <= number; i++){
+    arr.push(i);
+  }
+
+  return arr.reduce((sum, current) => sum * current, 1)
 };
 
 // Do not edit below this line
